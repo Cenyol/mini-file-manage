a694b813a14ee5e6e9dbda5e9a1685cc3ceaed33
cenyol <cenyol@163.com>
2020-08-01 14:44:45 +0800

    craete and update page html

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const nunjucks = require('nunjucks')
 const path = require('path')
 let app = express()
 
+var bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json())
+
 nunjucks.configure(path.resolve(__dirname,'view'),{
   autoescape:true,
   express:app
-})
+});
 
 //创建数据库文件
-let database = new sqlite3.Database("sqlite3.db", function (err) {
+let database = new sqlite3.Database("sqlite3.db", function(err) {
   if (err) {
       console.log("new database error,", err.message);
       process.exit();
@@ -18,20 +22,32 @@ let database = new sqlite3.Database("sqlite3.db", function (err) {
 });
 
 
-app.get('/',function(req,res){
+app.get('/', function(req,res) {
   let data = {};
 
   //查询
-  database.all("select * from file_info", function (err, files) {
+  database.all("select * from file_info", function(err, files) {
     if (err) {
         console.log("select from file_info error,", err.message);
     } else {
       data['files'] = files
-      res.render('user.html', data);
+      res.render('index.html', data);
     }
   });
-})
+});
 
-app.listen(8080)
 
-console.log('server is running in 8080...')
+app.get('/form', function(req, res) {
+  res.render('form.html');
+});
+app.post('/form', function(req, res) {
+  console.log(req.body);
+  // console.log(req.body.content);
+  // var result = req.body
+  //   res.send(JSON.stringify(result));
+});
+
+
+app.listen(8080);
+
+console.log('server is running in 8080...');
